d53bfd7bb7d5f27f4275640f0ea78345715b500d
Saad Toumi Addad <saadtoumi098@gmail.com>
2026-01-07 17:30:22 +0100

    Update feed.php
    Comentar codigo.

--- feed.php
+++ feed.php
@@ -1,17 +1,26 @@
 <?php
+// Carga de seguridad, base de datos y diseño de cabecera
 require '../includes/auth.php';
 require '../config/db.php';
 include '../includes/header.php';
 
-
+// Obtiene todas las publicaciones junto con el nombre de su autor mediante un JOIN, ordenadas por fecha
 $posts = $pdo->query("SELECT posts.*, users.username FROM posts JOIN users ON posts.user_id = users.id ORDER BY created_at DESC")->fetchAll();
 ?>
+
 <a href="create_post.php">Nuevo post</a>
+
 <?php foreach ($posts as $post): ?>
 <article>
-<h2><a href="post.php?id=<?= $post['id'] ?>"><?= htmlspecialchars($post['title']) ?></a></h2>
-<p><?= substr(htmlspecialchars($post['content']), 0, 100) ?>...</p>
-<small>Por <?= $post['username'] ?></small>
+    <h2><a href="post.php?id=<?= $post['id'] ?>"><?= htmlspecialchars($post['title']) ?></a></h2>
+    
+    <p><?= substr(htmlspecialchars($post['content']), 0, 100) ?>...</p>
+    
+    <small>Por <?= $post['username'] ?></small>
 </article>
 <?php endforeach; ?>
-<?php include '../includes/footer.php'; ?>
+
+<?php 
+// Carga el pie de página
+include '../includes/footer.php'; 
+?>

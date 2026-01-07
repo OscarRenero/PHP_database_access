96fb5db94d1094f382e64ce130cbbd1dc3b308ab
Saad Toumi Addad <saadtoumi098@gmail.com>
2026-01-07 17:25:19 +0100

    Update auth.php
    Se comenta el codigo.

--- auth.php
+++ auth.php
@@ -1,6 +1,8 @@
 <?php
-session_start();
+session_start(); // Inicia o reanuda la sesión del usuario
+
+// Verifica si el usuario está autenticado; de lo contrario, redirige al login
 if (!isset($_SESSION['user_id'])) {
-header('Location: login.php');
-exit;
+    header('Location: login.php');
+    exit;
 }

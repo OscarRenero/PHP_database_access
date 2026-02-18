3ecbe05a2e98a015584e0b8b561b3eabb342f6ef
Saad Toumi Addad <saadtoumi098@gmail.com>
2026-02-18 16:08:36 +0100

    Update database.sql

--- sql/database.sql
+++ sql/database.sql
@@ -1,6 +1,6 @@
 -- Active: 1767893039539@@127.0.0.1@3306
 -- Creación de la base de datos para el foro de relojes
-USE WatchYourPost;
+USE Ghana;
 
 -- Tabla para gestionar los usuarios de la comunidad
 CREATE TABLE usaad (
@@ -34,4 +34,5 @@ CREATE TABLE comments (
 );
 
 ALTER TABLE users ADD COLUMN avatar INT DEFAULT 1 AFTER password;
+
 
